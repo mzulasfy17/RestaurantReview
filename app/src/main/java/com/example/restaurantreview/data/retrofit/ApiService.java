6d164a420d9deebc7824bbc4e3e19a4667ccac4d
Zul <m.zulasfi17@gmail.com>
2024-04-21 19:15:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/restaurantreview/data/retrofit/ApiService.java
@@ -0,0 +1,25 @@
+package com.example.restaurantreview.data.retrofit;
+
+import com.example.restaurantreview.data.Response.PostReviewResponse;
+import com.example.restaurantreview.data.Response.RestaurantResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface ApiService {
+    @GET("detail/{id}")
+    Call<RestaurantResponse> getRestaurant(@Path("id") String id);
+
+    @FormUrlEncoded
+    @Headers({"Authorization: token 12345"})
+    @POST("review")
+    Call<PostReviewResponse> postReview(
+            @Field("id") String id,
+            @Field("name") String name,
+            @Field("review") String review);
+}
